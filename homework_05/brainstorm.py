2a2fd62491f4cbbe6c8588a7743ddf6a0291f48f
Sabine <skchang@berkeley.edu>
2025-10-22 23:29:02 -0700

    done with hw5

---
+++ homework_05/brainstorm.py
@@ -0,0 +1,21 @@
+Brainstorm:
+ 1). Microlensing/blakcholes, binary star systems, rocket propulsion, star 
+luminocity and oribtal motion
+ 2).- https://www.astronexus.com/projects/hyg ( Data set of star 
+information consisting of naked eye data and comprehensive and 
+high-acuracy, including a range of distances and provides positon, names 
+brightness and spectral information).
+    - https://catalog.data.gov/dataset/binary-neutron-star-merger( Data 
+set includign waveform data for various binary systems, inclduing neutron 
+stars and black hole mergers)
+    - 
+https://data.nasa.gov/dataset/maven-magnetometer-mag-magnetic-field-and-orbital-position-sun-state-and-payload-coordinat 
+(Data sets including infromation on electric and magnetic feilds and 
+plasma environments relating to orbital position).
+
+ 3). Using dictornaries to hold data and sort data for with relevant 
+indicators
+
+ 4). Creating a dictionary of star infroamtion and sorting/ graphing the 
+revlevant information. Creating a simulation of star movement or of 
+waveforms.
